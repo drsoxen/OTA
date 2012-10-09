088b69e28bbfdb541d073033301e9616f177b71e
Chris Ziraldo <chris@Chriss-Mac-mini.local>
2012-10-09 13:56:34 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Install iOS App</title>
+<meta name="viewport" id="viewport" content="width=320, user-scalable=no, minimum-scale=1, maximum-scale=1" />
+
+<style>
+body {
+	font-family: "Helvetica";
+	padding: 10px;
+}
+
+
+</style>
+</head>
+<body>
+
+<center>
+	<a href="itms-services://?action=download-manifest&url=http://drsoxen.com/AdHoc/myApp.plist"><img src="http://energy.gov/sites/prod/files/styles/article_hero/public/green-Download256_1.png" ></a>
+	<br />
+	<br />
+	<a href="iOS_Team_Provisioning_Profile.mobileprovision">Install Team Provisioning Profile</a></li>
+</center>
+</ul>
+</body>
+</html>
